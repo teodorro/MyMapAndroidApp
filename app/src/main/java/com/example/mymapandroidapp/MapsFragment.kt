80cf177dfdf25492b07ee0aec15d996012b54ab7
teodorro <edem.teo@gmail.com>
2021-07-19 15:59:43 +0300

    .

---
+++ app/src/main/java/com/example/mymapandroidapp/MapsFragment.kt
@@ -0,0 +1,107 @@
+package com.example.mymapandroidapp
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
+
+class MapsFragment : Fragment() {
+
+    private lateinit var googleMap: GoogleMap
+
+    @SuppressLint("MissingPermission")
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                googleMap.apply {
+                    isMyLocationEnabled = true
+                    uiSettings.isMyLocationButtonEnabled = true
+                }
+            } else {
+                // TODO: show sorry dialog
+            }
+        }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        lifecycle.coroutineScope.launchWhenCreated {
+            googleMap = mapFragment.awaitMap().apply {
+                isTrafficEnabled = true
+                isBuildingsEnabled = true
+
+                uiSettings.apply {
+                    isZoomControlsEnabled = true
+                    setAllGesturesEnabled(true)
+                }
+            }
+
+            when {
+                // 1. Проверяем есть ли уже права
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    googleMap.apply {
+                        isMyLocationEnabled = true
+                        uiSettings.isMyLocationButtonEnabled = true
+                    }
+
+                    val fusedLocationProviderClient = LocationServices
+                        .getFusedLocationProviderClient(requireActivity())
+
+                    fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                        println(it)
+                    }
+                }
+                // 2. Должны показать обоснование необходимости прав
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    // TODO: show rationale dialog
+                }
+                // 3. Запрашиваем права
+                else -> {
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        }
+    }
+}
